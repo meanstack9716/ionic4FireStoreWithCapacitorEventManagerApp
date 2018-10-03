3698f6cc2644395e26c26556654e4a34b93070c6
meanstack9 <meanstack9@gmail.com>
2018-10-03 09:19:39 +0530

    created form for add new events, and its functionality

--- src/app/pages/event-create/event-create.page.ts
+++ src/app/pages/event-create/event-create.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EventService } from '../../services/event/event.service';
 
 @Component({
   selector: 'app-event-create',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-create.page.scss'],
 })
 export class EventCreatePage implements OnInit {
+  constructor(private router: Router, private eventService: EventService) { }
 
-  constructor() { }
+  ngOnInit() { }
 
-  ngOnInit() {
+  createEvent(
+    eventName: string,
+    eventDateObject: any,
+    eventPrice: number,
+    eventCost: number
+  ): void {
+    if (eventDateObject === undefined) {
+      return;
+    } else if (
+      eventDateObject.year === undefined ||
+      eventDateObject.month === undefined ||
+      eventDateObject.day === undefined
+    ) {
+      return;
+    }
+    const eventDate: Date = new Date(
+      eventDateObject.year.value,
+      eventDateObject.month.value - 1,
+      eventDateObject.day.value
+    );
+    this.eventService
+      .createEvent(eventName, eventDate, eventPrice, eventCost)
+      .then(() => {
+        this.router.navigateByUrl('');
+      });
   }
-
 }
