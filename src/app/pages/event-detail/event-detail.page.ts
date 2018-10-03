a85b031a6d0a33310991d80c93ca448d0980b2bf
meanstack9 <meanstack9@gmail.com>
2018-10-03 09:26:06 +0530

    create design for event detail page and add guest option with transaction

--- src/app/pages/event-detail/event-detail.page.ts
+++ src/app/pages/event-detail/event-detail.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from '../../services/event/event.service';
+import { ActivatedRoute } from '@angular/router';
+import { Plugins, CameraResultType } from '@capacitor/core';
+
+const { Camera } = Plugins;
 
 @Component({
   selector: 'app-event-detail',
@@ -6,10 +11,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-detail.page.scss'],
 })
 export class EventDetailPage implements OnInit {
-
-  constructor() { }
+  public currentEvent: any = {};
+  public guestName = '';
+  public guestPicture: string = null;
+  constructor(private eventService: EventService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    const eventId: string = this.route.snapshot.paramMap.get('id');
+    this.eventService
+      .getEventDetail(eventId)
+      .get()
+      .then(eventSnapshot => {
+        this.currentEvent = eventSnapshot.data();
+        this.currentEvent.id = eventSnapshot.id;
+      });
   }
 
+  addGuest(guestName: string): void {
+    this.eventService
+      .addGuest(
+        guestName,
+        this.currentEvent.id,
+        this.currentEvent.price,
+        this.guestPicture
+      )
+      .then(() => {
+        this.guestName = '';
+        this.guestPicture = null;
+      });
+  }
+
+  async takePicture(): Promise<void> {
+    try {
+      const profilePicture = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.Base64,
+      });
+      this.guestPicture = profilePicture.base64Data.slice(23);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
