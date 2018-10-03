8731f98306b9731bff48d52902b1c222773b40f3
meanstack9 <meanstack9@gmail.com>
2018-10-03 08:41:19 +0530

    created login page design and forms

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,13 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  FormGroup,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+import { LoadingController, AlertController } from '@ionic/angular';
+import { AuthService } from '../../services/user/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  public loginForm: FormGroup;
+  public loading: HTMLIonLoadingElement;
+  constructor(
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController,
+    private authService: AuthService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: ['',
+        Validators.compose([Validators.required, Validators.email])],
+      password: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(6)]),
+      ],
+    });
+  }
 
   ngOnInit() {
   }
