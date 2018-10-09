9c8d9e84b46bc8f9e1196d40dceb2c1504db6844
meanstack9 <meanstack9@gmail.com>
2018-10-09 09:21:40 +0530

    capacitor changes

--- src/app/config/credentials.ts
+++ src/app/config/credentials.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: 'AIzaSyD304qlVfT4gHsjcNkAR9vEINTKtg4QJtc',
-    authDomain: 'ionic4fscapeventmanagerapp.firebaseapp.com',
-    databaseURL: 'https://ionic4fscapeventmanagerapp.firebaseio.com',
-    projectId: 'ionic4fscapeventmanagerapp',
-    storageBucket: 'ionic4fscapeventmanagerapp.appspot.com',
-    messagingSenderId: '729393331628'
+    apiKey: 'xxxxxxxxxx',
+    authDomain: 'xxxxxxxxxx',
+    databaseURL: 'xxxxxxxxxx',
+    projectId: 'xxxxxxxxxx',
+    storageBucket: 'xxxxxxxxxx',
+    messagingSenderId: 'xxxxxxxxxx'
 };
